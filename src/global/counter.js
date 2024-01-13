cca3fab3442f921dc9d47bcc41d83dccbf3dae47
lukepark327 <lukepark327@gmail.com>
2024-01-14 00:42:21 +0900

    feat: Global Counter micro service

---
+++ src/global/counter.js
@@ -0,0 +1,54 @@
+const crypto = require('crypto');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+// State
+let epoch = 10000; // (ms)
+let round = 0;
+const INIT = "Hello, World!";
+let seed = sha256(`${INIT}${round}`);
+
+const app = express();
+app.use(bodyParser.json());
+
+app.get('/epoch', async (req, res) => {
+    try {
+        res.status(200).send({ "epoch": epoch });
+    } catch (error) {
+        res.status(500).send({ "error": error.message });
+    }
+});
+
+app.get('/round', async (req, res) => {
+    try {
+        res.status(200).send({ "round": round });
+    } catch (error) {
+        res.status(500).send({ "error": error.message });
+    }
+});
+
+app.get('/seed', async (req, res) => {
+    try {
+        res.status(200).send({ "seed": seed });
+    } catch (error) {
+        res.status(500).send({ "error": error.message });
+    }
+});
+
+function sha256(message) {
+    return crypto.createHash('sha256').update(message).digest('hex');
+}
+
+function repeatFunction() {
+    seed = sha256(`${seed}${round}`);
+    round++;
+    console.log(`Round: ${round}, Seed: ${seed}`);
+}
+
+setInterval(repeatFunction, epoch);
+
+const port = process.env.PORT || 30328;
+app.listen(port, () => {
+    console.log(`Counter service is running on port ${port}`);
+    console.log(`Round: ${round}, Seed: ${seed}`);
+});
