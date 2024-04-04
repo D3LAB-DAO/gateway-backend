96708a784ba6c30c75c06c8441f9e155564d5021
lukepark327 <lukepark327@gmail.com>
2024-04-04 12:57:06 +0900

    feat: now receive api returns result, too

--- src/relay/receive.js
+++ src/relay/receive.js
@@ -1,14 +1,45 @@
 const { createConnection, closeConnection } = require('./db/maria');
-const { createTable, saveRequest } = require('./db/queries');
+const { createTable, saveRequest, getResultsById } = require('./db/queries');
 const axios = require('axios');
 const express = require('express');
 const bodyParser = require('body-parser');
+const { assert } = require('elliptic/lib/elliptic/utils');
+
+const TIMEOUT = 30000;
+const EPOCH = 3000;
 
 const COUNTER_ENDPOINT = "http://localhost:30328";
 let connection;
 
 const app = express();
 app.use(bodyParser.json());
+
+
+const pollWithTimeout = (pollingFunction, interval, timeout) => {
+    return new Promise((resolve, reject) => {
+        const timeoutId = setTimeout(() => {
+            clearInterval(intervalId);
+            console.log('Polling timed out. Interval cleared.');
+            reject('Timeout'); // Reject the promise on timeout
+        }, timeout);
+
+        const intervalId = setInterval(async () => {
+            try {
+                const result = await pollingFunction();
+                if (result && result != {}) {
+                    clearTimeout(timeoutId);
+                    clearInterval(intervalId);
+                    resolve(result); // Resolve the promise when the task completes
+                }
+            } catch (error) {
+                clearTimeout(timeoutId);
+                clearInterval(intervalId);
+                reject(error); // Reject the promise on error
+            }
+        }, interval);
+    });
+};
+
 
 app.post('/request', async (req, res) => {
     try {
@@ -24,7 +55,7 @@ app.post('/request', async (req, res) => {
         const _response = await axios.get(COUNTER_ENDPOINT + "/seed");
         const seed = _response.data.seed;
 
-        const result = await saveRequest(connection, {
+        const savedRequest = await saveRequest(connection, {
             "uri": uri,
             "params": JSON.stringify(params),
             "timestamp": timestamp,
@@ -34,7 +65,35 @@ app.post('/request', async (req, res) => {
             "max_retries": 10,
         });
 
-        res.status(200).send({ "id": result.insertId });
+        const getResult = async () => {
+            try {
+                const job = (await getResultsById(connection, savedRequest.insertId))[0];
+                // console.log(savedRequest.insertId, ">", job);
+                if (job != undefined) {
+                    return job.result;
+                }
+                else {
+                    assert("No result");
+                }
+            } catch (error) {
+                // console.error(error);
+                return error;
+            }
+        }
+        pollWithTimeout(getResult, EPOCH, TIMEOUT)
+            .then((result) => {
+                // console.log(result);
+                res.status(200).send({
+                    "id": savedRequest.insertId,
+                    "result": result
+                });
+            })
+            .catch((error) => {
+                // console.log(error);
+                res.status(200).send({
+                    "id": savedRequest.insertId
+                });
+            });
     } catch (error) {
         console.error(error);
         res.status(500).send({ "error": error.message });
